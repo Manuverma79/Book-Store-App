f872f089aae94807d0c7eb3d0dcbd9c0dcb456db
Manuverma79 <95175647+Manuverma79@users.noreply.github.com>
2024-05-26 13:47:56 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { MdMenuBook } from "react-icons/md";
 import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <div className="sticky top-0 navbar bg-base-200">
+    <div className="sticky top-0 z-10 navbar bg-base-200">
       <Link to="/" className="text-xl btn btn-ghost">
         <div className="flex items-center gap-2 font-bold">
           <MdMenuBook className="text-2xl" />
